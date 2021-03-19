592910016ced81d475df514db6d9e40311cfdfbc
hunterachieng <79210550+hunterachieng@users.noreply.github.com>
2021-03-19 12:45:33 +0300

    Update nested functions.js

--- nested functions.js
+++ nested functions.js
@@ -2,7 +2,7 @@ function names (){
  var name1 = "Hunter";
  var name2 = "Pamera";
  function friends(){
-     console.log(name1 + " "+ name2 + " are friends.");
+     console.log(name1 + " and "+ name2 + " are friends.");
  }
  friends();
 }
